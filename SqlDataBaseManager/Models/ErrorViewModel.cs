45188ed58a4405591491cafe6c342855f109ba2f
Orwas5 <33007905+Waksuu@users.noreply.github.com>
2019-02-20 15:49:42 +0100

    Add project files.

---
+++ SqlDataBaseManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SqlDataBaseManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
