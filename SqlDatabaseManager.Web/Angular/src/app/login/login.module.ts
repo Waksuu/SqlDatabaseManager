b18aa9518e8b76566ab4b555fccdc5b86d4aa609
Orwas5 <33007905+Waksuu@users.noreply.github.com>
2019-04-18 16:41:14 +0200

    Code clean-up

--- SqlDatabaseManager.Web/Angular/src/app/login/login.module.ts
+++ SqlDatabaseManager.Web/Angular/src/app/login/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
-import { LoginService } from './login.service';
 import { LoginComponent } from './login.component';
+import { LoginService } from './login.service';
 
 @NgModule({
   declarations: [
