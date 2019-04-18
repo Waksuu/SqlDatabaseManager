66c71988c2a5e7c757b2285f52991a6bb68af442
Orwas5 <33007905+Waksuu@users.noreply.github.com>
2019-04-18 18:24:23 +0200

    Improve routing

--- SqlDatabaseManager.Web/Angular/src/app/app-routing.module.ts
+++ SqlDatabaseManager.Web/Angular/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DatabaseComponent } from './database/database.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'database', component: DatabaseComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- SqlDatabaseManager.Web/Angular/src/app/app.module.ts
+++ SqlDatabaseManager.Web/Angular/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DatabaseModule } from './database/database.module';
 import { LoginModule } from './login/login.module';
+import { DatabaseComponent } from './database/database.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
